212aba65fee6804ff40745418b175d3f78a6d341
Shenal Fernando <developershenal@gmail.com>
2023-05-09 02:26:27 +0530

    Change to compatible with formik

--- src/components/UI/Forms/Input.jsx
+++ src/components/UI/Forms/Input.jsx
@@ -1,22 +1,22 @@
+import { useField } from "formik";
+import InputError from "./InputError";
+
 function Input(props) {
+  const [field, meta] = useField(props.name);
   return (
     <>
-      <label htmlFor={props.id} className="font-medium text-gray-700">
+      <label htmlFor={props.id} className="text-gray-700">
         {props.labelName}
       </label>
       <input
         type={props.type ? props.type : "text"}
         id={props.id}
-        onChange={props.onChange ? props.onChange : undefined}
-        onBlur={props.onBlur ? props.onBlur : undefined}
-        name={props.name}
-        defaultValue={props.value ? props.value : undefined}
+        placeholder={props.placeholder && props.placeholder}
         className={`border border-gray-300 px-4 py-2 rounded ${props.className ? props.className : undefined}`}
-        required={props.required == false ? false : true}
-        pattern={props.pattern ? props.pattern : undefined}
-        placeholder={props.placeholder ? props.placeholder : undefined}
+        {...field}
         {...props.attributes}
       />
+      {meta.error && meta.touched && <InputError message={meta.error} />}
     </>
   );
 }
